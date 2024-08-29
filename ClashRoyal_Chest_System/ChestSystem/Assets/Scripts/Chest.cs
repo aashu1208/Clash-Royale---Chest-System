2ed12216bfd8b5db61441d0918ee048fbb45be42
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-08-29 21:02:39 +0530

    Setting each, chest and its state

--- ClashRoyal_Chest_System/ChestSystem/Assets/Scripts/Chest.cs
+++ ClashRoyal_Chest_System/ChestSystem/Assets/Scripts/Chest.cs
@@ -6,5 +6,18 @@ public enum ChestState { Locked, Unlocking, Unlocked, Collecting}
 
 public class Chest
 {
-    
+    public ChestState State {get; private set;}
+    public int Coins { get; private set; }
+
+    public int Gems { get; private set; }
+
+    public float UnlockTime { get; private set; }
+    public float remainingTime;
+
+    public Chest(int coins, int gems, float unlocktime)
+    {
+
+
+    }
+
 }
