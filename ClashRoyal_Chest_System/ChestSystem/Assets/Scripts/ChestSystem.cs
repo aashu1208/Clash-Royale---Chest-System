26f4fba1d2dcb641f705549820a31468803d2cb7
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-08-30 16:00:00 +0530

    updated

--- ClashRoyal_Chest_System/ChestSystem/Assets/Scripts/ChestSystem.cs
+++ ClashRoyal_Chest_System/ChestSystem/Assets/Scripts/ChestSystem.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class ChestSystem : MonoBehaviour
 {
+    public DesignerConfig config;
+    private List<ChestSlot> slots;
+    private Queue<Chest> chestQueue;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        slots = new List<ChestSlot>(FindObjectsOfType<ChestSlot>());
+        chestQueue = new Queue<Chest>(config.MaxQueue);
     }
 
     // Update is called once per frame
