856e8e872804226b42491816ce134e901dba87a2
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-08-30 22:40:25 +0530

    uis and scriptables objects updated with ranges

--- ClashRoyal_Chest_System/ChestSystem/Assets/Scripts/DesignerConfig.cs
+++ ClashRoyal_Chest_System/ChestSystem/Assets/Scripts/DesignerConfig.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="DesignerConfig", menuName ="GameConfig/DesignerConfig")]
 public class DesignerConfig : ScriptableObject
 {
-    public int[] CoinRanges = new int[]{ 100, 200, 300, 500, 600, 800, 1000, 1200};
-    public int[] GemRanges = new int[] { 10, 20, 20, 40, 45, 60, 80, 100};
+    public int[] CoinRanges = new int[8];
+    public int[] GemRanges = new int[8];
     public int MaxSlot;
     public int MaxQueue;
     public int GemCostPerTenMinutes;
