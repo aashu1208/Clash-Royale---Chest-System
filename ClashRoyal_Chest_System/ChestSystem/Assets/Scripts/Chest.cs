04aac886fbd3a6bdda4c5e5992757bc6399d44a0
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-09-10 00:15:30 +0530

    Chest script in progress

--- ClashRoyal_Chest_System/ChestSystem/Assets/Scripts/Chest.cs
+++ ClashRoyal_Chest_System/ChestSystem/Assets/Scripts/Chest.cs
@@ -2,51 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ChestState { Locked, Unlocking, Unlocked, Collected}
 
+public enum ChestState
+{
+    Locked,
+    Unlocking,
+    Unlcoked,
+    Collected
+
+
+}
 public class Chest
 {
-    public ChestState State {get; private set;}
+    public ChestState State;
     public int Coins { get; private set; }
-
     public int Gems { get; private set; }
-
     public float UnlockTime { get; private set; }
-    public float remainingTime;
+    public float RemainingTime { get; private set; }
 
-    public Chest(int coins, int gems, float unlocktime)
+    public void TapToStart()
     {
-        Coins = coins;
-        Gems = gems;
-        UnlockTime = unlocktime;
-        remainingTime = unlocktime;
-        State = ChestState.Locked;
-
-    }
-    public void StartUnlocking()
-    {
-        State = ChestState.Unlocking;
-    }
-
-    public void UpdateUnlocking(float deltaTime)
-    {
-        if (State == ChestState.Unlocking)
+        if (State == ChestState.Locked)
         {
-            remainingTime -= deltaTime;
-            if (remainingTime <= 0)
-            {
-                State = ChestState.Unlocked;
-            }
+            State = ChestState.Unlocking;
         }
-    }
-    public int CalculateGemCost()
-    {
-        return Mathf.CeilToInt(remainingTime/600); //1 gem per 10 minutes
-    }
-    public void Collect()
-    {
-        State = ChestState.Collected;
-
     }
 
 }
