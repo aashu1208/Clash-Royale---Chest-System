85cbd1cf9c183a3ad3a52e8eeb2b2b82e42b135f
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-09-11 23:22:18 +0530

    arrays for chests and texts created

--- ClashRoyal_Chest_System/ChestSystem/Assets/Scripts/Chest.cs
+++ ClashRoyal_Chest_System/ChestSystem/Assets/Scripts/Chest.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 
 public enum ChestState
@@ -21,33 +23,36 @@ public class Chest
     public float UnlockTime { get; private set; }
     public float RemainingTime { get; private set; }
     private ChestData chestData;
-    public Chest(ChestData data)
+
+    /*public Chest(ChestData data)
     {
         chestData = data;
         State = ChestState.Locked;
 
-        Coins = Random.Range(data.minCoins, data.minCoins+1);
+        Coins = Random.Range(chestData.minCoins, chestData.minCoins+1);
         Gems = Random.Range(data.minGems, data.maxGems+1);
         
 
-    }
-
-    public void TapToStart()
+    }*/
+    public void TapToStart(int chestIndex)
     {
         if (State == ChestState.Locked)
         {
             State = ChestState.Unlocking;
+            ChestManager.chestManagerInstance.chestTexts[chestIndex].text = "Unlocking..";
             Debug.Log("Unlocking....");
         }
 
         else if (State == ChestState.Unlocking)
         {
             State = ChestState.Unlocked;
+            ChestManager.chestManagerInstance.chestTexts[chestIndex].text = "Unlocked";
             Debug.Log("Unlocked..");
         }
         else if (State == ChestState.Unlocked)
         {
             State = ChestState.Collected;
+            ChestManager.chestManagerInstance.chestTexts[chestIndex].text = "Collected";
             Debug.Log("Collected..");
         }
     }
--- ClashRoyal_Chest_System/ChestSystem/Assets/Scripts/ChestManager.cs
+++ ClashRoyal_Chest_System/ChestSystem/Assets/Scripts/ChestManager.cs
@@ -1,15 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ChestManager : MonoBehaviour
 {
-    public Chest chest;
+    public static ChestManager chestManagerInstance;
+    public Chest[] chest;
     public ChestState cheststate;
+
+    // Texts UI's
+    public TextMeshProUGUI[] chestTexts;
+   
     // Start is called before the first frame update
     void Start()
     {
-        //chest = new Chest();
+        chest = new Chest[4];
+        for (int i = 0; i < chest.Length; i++)
+        {
+            chest[i] = new Chest();
+        }
+
+        if (chestManagerInstance == null)
+        {
+            chestManagerInstance = this;
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
@@ -18,9 +39,12 @@ public class ChestManager : MonoBehaviour
         
     }
 
-    public void OnClick_TapToStart()
+    public void OnClick_TapToStart(int chestIndex)
     {
-        chest.TapToStart();
+        if (chestIndex >= 0 && chestIndex < chest.Length)
+        {
+            chest[chestIndex].TapToStart(chestIndex);
+        }
 
     }
 }
